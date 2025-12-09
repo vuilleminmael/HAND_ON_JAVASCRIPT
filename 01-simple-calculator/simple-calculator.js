1ec526497fed793728ed45101843e9dbbdf5c2e9
vuilleminmael <mael.cityhandmini@gmail.com>
2025-12-09 17:18:02 +0100

    change code

---
+++ 01-simple-calculator/simple-calculator.js
@@ -0,0 +1,28 @@
+let number1Element = document.getElementById("number1");
+let number2Element = document.getElementById("number2");
+let operatorElement = document.getElementById('operator');
+console.log("test", number1Element);
+
+function calculer(number1, number2, operator) {
+    const num1 = parseFloat(number1.value);
+    const num2 = parseFloat(number2.value);
+    const op = operator.value;
+    
+    switch (op) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            if (num2 === 0) {
+                return "Erreur : division par zero";
+            }
+            return num1 / num2;
+        default:
+            return "operator non reconnu";
+    }
+}
+
+console.log(calculer(number1Element,number2Element,operatorElement))
