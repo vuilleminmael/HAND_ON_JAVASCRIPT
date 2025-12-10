c4bdfd5f2446a017ad4330a137157873b627221a
vuilleminmael <mael.cityhandmini@gmail.com>
2025-12-10 11:12:37 +0100

    finish project

--- 01-simple-calculator/simple-calculator.js
+++ 01-simple-calculator/simple-calculator.js
@@ -26,3 +26,7 @@ function calculer(number1, number2, operator) {
 }
 
 console.log(calculer(number1Element,number2Element,operatorElement))
+document.getElementById("btn").addEventListener("click", fonction (){
+    const resultat = calculer(number1Element,number2Element,operatorElement);
+    resultatElement.texteContent = "Resultat : " + resultat;
+});
